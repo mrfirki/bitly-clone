61a3e94643024f590bf4e96b5501942461efeb24
mrfirki <mrfikri94@gmail.com>
2017-03-13 10:17:43 +0800

    adding heroku support into framework

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -8,10 +8,25 @@ end
 
 post '/urls' do
   # create a new Url
+  # new_url = Url.new(long_url: params[:long_url])
+  # if new_url.valid?
+  #   new_url.save
+  #   return new_url.to_json
+  # else
+  # 	json_hash = {}
+  #   json_hash[:error_msgs] = new_url.errors.messages[:long_url][0]
+
+  #   if json_hash[:error_msgs] == "has already been taken"
+  #   	find_url = Url.find_by(long_url: params[:long_url])
+  #   	json_hash[:short_url] = find_url.short_url
+  #   	json_hash[:long_url] = find_url.long_url
+  #   end
+
+  #   return json_hash.to_json
+  # end
 	@url = Url.create(long_url: params[:long_url])
-	# byebug
 	redirect :"/"
-	# erb :"static/index"
+	erb :"static/index"
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
-  $(".table").click(function() {
-  	$(this).fadeOut();
-  });
+  // $(".head").click(function() {
+  // 	$(this).fadeOut()
+  // });
+  $("text").click(function(){
+    $("p").hide("slow", function(){
+        alert("The paragraph is now hidden");
+    });
 });
+});
+
+	  // this ID here refers to the form where the usesr types in a URL, you may have a different name for the ID. Edit the code accordingly.
+// 	  $('#url-form').submit(function(e){ 
+// 	    e.preventDefault();         
+// 	    $.ajax({
+// 	      url: '/urls', //this refers to the route post '/url'
+// 	      method: 'POST',
+// 	      data: $(this).serialize(),
+// 	      success: function(data){
+// 	     	data = JSON.parse(data)
+// 	     	short_link = data.short_url
+// 	     	$('#ajax-show').append()
+// 	      // write some code here to display the shortened URL
+// 	     }
+// 	    }); // end of function .ajax
+// 	  e.preventDefault()
+// 	}); // end of function .submit
+// }); // end of function document.ready
+
+
+
+ // $('#user-form').submit(function(e){
+ //    $.ajax({
+ //      url: '/urls',
+ //      method: 'POST',
+ //      data: $(this).serialize(),
+ //      success: function(data){
+ //        data = JSON.parse(data)
+ //        short_link = data.short_url
+ //        $('#ajax-show').append(`<p>https://ariefbitly.herokuapp.com/${short_link}</p>`)
+ //      }
+ //    });
+ //    e.preventDefault()
+ //  });
+// });
